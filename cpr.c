9154a0846feba6ea4fd0726955a1431bc995068d
mdafzalkhan <mdafzalkhan20112000@gmail.com>
2025-09-21 19:15:05 +0530

    c basic code

--- cpr.c
+++ cpr.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
-int main(){
-    printf("hellow afzal how are you. keep going one day we will get success. kept persistency and be focused ");
+int main() {
+    int i;
+
+    printf("hello afzal, how are you? keep going—one day we will get success. keep persistence and stay focused.\n");
+
+    for (i = 0; i < 4; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
 }
